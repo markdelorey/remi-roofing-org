8ab1f6d1fea237b8bd5a13cae6fd13b93e128c5e
Mark Delorey <markdelorey@MacBook-Pro-5.local>
2025-09-29 15:55:01 -0600

    feat: add remi-roofing-org-core shared package

---
+++ packages/remi-roofing-org-core/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Example function.
+ *
+ * @example
+ *
+ * Use from other apps/packages:
+ *
+ * ```ts
+ * import { hello } from '@repo/remi-roofing-org-core'
+ *
+ * hello()
+ * ```
+ */
+export function hello() {
+	return 'Hello, world!'
+}
