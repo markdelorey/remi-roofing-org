5552910db41a487586f1f17b38ada67b2dba1431
Mark Delorey <markdelorey@mac.lan>
2025-09-29 20:08:13 -0600

    fix: Correct vite dev command and resolve path aliases

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,12 +1,14 @@
 import { cloudflare } from '@cloudflare/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
 		cloudflare(),
+		tsconfigPaths(),
 		{
 			name: 'logging',
 			handleHotUpdate({ file, server }) {
